22500648fb07ad67afc97556a8a1f32da3ec5b92
c3730915 <1023700418@qq.com>
2020-10-07 15:00:14 +0800

    输出数据到浏览器

---
+++ src/cn/itcast/web/servlet/day15Response/ServletWriteToBrow.java
@@ -0,0 +1,27 @@
+package cn.itcast.web.servlet.day15Response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ServletWriteToBrow")
+public class ServletWriteToBrow extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        //获取字符串输出流
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("content-type","text/html;charset=utf-8"); //告诉浏览器当前的编码，防止中文乱码
+        //输出数据
+        writer.write("hi response 你好");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+
+    }
+}
