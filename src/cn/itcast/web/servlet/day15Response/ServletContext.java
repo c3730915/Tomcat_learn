271d710cb98fd8003a62195c752e77f8960f56e8
c3730915 <1023700418@qq.com>
2020-10-08 08:01:34 +0800

    修复验证码问题

---
+++ src/cn/itcast/web/servlet/day15Response/ServletContext.java
@@ -0,0 +1,19 @@
+package cn.itcast.web.servlet.day15Response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletContext")
+public class ServletContext extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
